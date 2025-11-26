7f4ebc90449b43da73d3c086edf1615b5bdc48ba
Kwazzi724 <dannyli724@gmail.com>
2025-11-26 20:58:22 +1100

    Normalise line endings

--- Assets/Scripts/TimelineEventReceiver.cs
+++ Assets/Scripts/TimelineEventReceiver.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Video;
 
 public class TimelineEventReceiver : MonoBehaviour
 {
+    [Header("Intro")] 
+    public GameObject hand;
+    public GameObject blackness;
+    public VideoPlayer videoPlayer;
+
+    [Header("Animated Objects")] 
+    public GameObject eye1;
+    public GameObject spikes1;
+    public GameObject girl1;
+    public float fadeInDuration;
+    
     [Header("Post Processing Volume")]
     public Volume volume;
 
@@ -35,7 +47,52 @@ public class TimelineEventReceiver : MonoBehaviour
             volume.profile.TryGet(out vignette);
         }
     }
+
+    public void ShowBlackness()
+    {
+        blackness.SetActive(true);
+    }
     
+    public void ShowEye1()
+    {
+        eye1.SetActive(true);
+    }
+    
+    public void ShowSpikes1()
+    {
+        spikes1.SetActive(true);
+    }
+    
+    public void ShowGirl1()
+    {
+        FadeInSpriteObject(girl1);
+    }
+    
+    public void HideIntro()
+    {
+        blackness.SetActive(false);
+        hand.SetActive(false);
+    }
+    
+    public void PlayVideo()
+    {
+        if (videoPlayer == null)
+            return;
+        videoPlayer.time = 0; // start from the beginning
+        videoPlayer.Play();
+    }
+    
+    public void StopVideo()
+    {
+        if (videoPlayer == null)
+            return;
+        videoPlayer.Stop();
+
+        // Optional: if you want it to stay at first frame instead of clearing:
+        // wooliesVideoPlayer.time = 0;
+        // wooliesVideoPlayer.Play();
+        // wooliesVideoPlayer.Pause();
+    }
 
     // fade in chromatic aberration
     public void TriggerChromaticAberrationFade()
@@ -144,6 +201,44 @@ public class TimelineEventReceiver : MonoBehaviour
         }
 
         vignette.intensity.Override(to);
+    }
+    
+    public void FadeInSpriteObject(GameObject targetObject)
+    {
+        SpriteRenderer spriteRenderer = targetObject.GetComponent<SpriteRenderer>();
+        targetObject.SetActive(true); 
+        StartCoroutine(FadeSpriteAlpha(spriteRenderer, 0f, 1f, fadeInDuration));
+    }
+
+    private IEnumerator FadeSpriteAlpha(SpriteRenderer spriteRenderer, float startAlpha, float endAlpha, float duration)
+    {
+        if (duration <= 0f)
+        {
+            Color instantColor = spriteRenderer.color;
+            instantColor.a = endAlpha;
+            spriteRenderer.color = instantColor;
+            yield break;
+        }
+
+        Color color = spriteRenderer.color;
+        color.a = startAlpha;
+        spriteRenderer.color = color;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+
+            color.a = Mathf.Lerp(startAlpha, endAlpha, t);
+            spriteRenderer.color = color;
+
+            yield return null;
+        }
+        // Snap to final value
+        color.a = endAlpha;
+        spriteRenderer.color = color;
     }
 }
 
